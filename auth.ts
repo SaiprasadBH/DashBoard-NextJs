ea52e8f221224dff0441bb756b12e2e420eae619
saiprasad <saiprasad.hegde@codecraft.co.in>
2024-08-28 09:43:29 +0530

    fixed eslint plugin issue

--- auth.ts
+++ auth.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import type { User } from "@/app/lib/definitions";
 import bcrypt from "bcrypt";
-
+import { use } from "react";
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
@@ -15,24 +15,22 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error("Failed to fetch user.");
   }
 }
-
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
       async authorize(credentials) {
-        // ...
-
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
-
           if (passwordsMatch) return user;
         }
-
-        console.log("Invalid credentials");
+        console.log("Invalid Credentials");
         return null;
       },
     }),
